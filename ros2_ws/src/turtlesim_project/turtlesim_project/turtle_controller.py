a5d29026d89aed36a56651cafcd4005212b776e6
KrisBoris <k.store@interia.eu>
2025-06-29 18:06:40 +0200

    #12 Turtlesim project
    Communication between nodes complete (publishers, subscribers, clients)
    ToDo: Turtle's position and angle setting

--- ros2_ws/src/turtlesim_project/turtlesim_project/turtle_controller.py
+++ ros2_ws/src/turtlesim_project/turtlesim_project/turtle_controller.py
@@ -3,6 +3,8 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+from std_msgs.msg import Bool
 
 
 class TurtleControllerNode(Node):
@@ -10,17 +12,31 @@ class TurtleControllerNode(Node):
     def __init__(self):
         super().__init__("turtle_controller")
 
+        self.__spawned_turtles = []
+
         self.__publisher_velocity = self.create_publisher(Twist, "turtle1/cmd_vel", 10)
-        self.__timer_velocity = self.create_timer(1.0, self.callback_timer_velocity)
+        self.__publisher_turtle_kill = self.create_publisher(Bool, "killed_turtle", 10)
+        self.__subscriber_spawned_turtle_position = self.create_subscription(Pose, "spawned_turtle", self.callback_subscription_spawned_turtle)   
+        self.__subscriber_turtle_postion = self.create_subscription(Pose, "/turtle1/pose", self.callback_subscription_turtle_position)     
         self.get_logger().info("turtle_controller node has been started")
 
-    def callback_timer_velocity(self):
+    def callback_subscription_spawned_turtle(self, msg: Pose):
+        self.__spawned_turtles.append(msg)    
+
+    def callback_subscription_turtle_position(self, turtle_postion: Pose):
         cmd_vel = Twist()
-        cmd_vel.linear.x = 1.0
-        cmd_vel.angular.z = 1.0
+
+        # Count direction -> set new angular velocity -> reach proper direction -> full speed
 
         self.__publisher_velocity.publish(cmd_vel)
 
+    def publish_turtle_killed(self):
+        msg = Bool()
+        msg.data = True
+
+        self.__spawned_turtles.pop(0)
+
+        self.__publisher_turtle_kill.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
--- ros2_ws/src/turtlesim_project/turtlesim_project/turtle_spawner.py
+++ ros2_ws/src/turtlesim_project/turtlesim_project/turtle_spawner.py
@@ -2,8 +2,11 @@
 
 import rclpy
 from rclpy.node import Node
+from turtlesim.msg import Pose
 from turtlesim.srv import Spawn
 from turtlesim.srv import Kill
+from std_msgs.msg import Bool
+from functools import partial
 import random
 import time
 
@@ -16,10 +19,13 @@ class TurtleSpawnerNode(Node):
 
         self.__spawn_pace = self.get_parameter("spawn_pace")
         self.__spawned_turtles = []
+
         self.__client_spawn = self.create_client(Spawn, "spawn")
-        self.__client_kill = self.create_client(Kill, "kill")
-        self.__spawn_timer = self.create_timer(3.0, self.callback_spawn_timer)
-        self.__kill_timer = self.create_timer(7.0, self.callback_kill_timer)
+        # self.__client_kill = self.create_client(Kill, "kill")
+        self.__publisher_spawned_turtle = self.create_publisher(Pose, "spawned_turtle", 10)
+        self.__subscriber_killed_turtle = self.create_subscription(Bool, "killed_turtle", self.callback_subscription_killed_turtle)
+        self.__spawn_timer = self.create_timer(self.__spawn_pace, self.callback_spawn_timer)
+        # self.__kill_timer = self.create_timer(7.0, self.callback_kill_timer)
 
         random.seed(int(round(time.time())))
         self.get_logger().info("turtle_spawner node has been started")
@@ -33,7 +39,7 @@ class TurtleSpawnerNode(Node):
         new_turtle.y = random.uniform(0, 10)    
 
         future = self.__client_spawn.call_async(new_turtle)
-        future.add_done_callback(self.__callback_service_spawn)        
+        future.add_done_callback(partial(self.__callback_service_spawn, request=new_turtle))           
 
     def callback_kill_timer(self):
         if len(self.__spawned_turtles) > 0:
@@ -46,13 +52,31 @@ class TurtleSpawnerNode(Node):
             future = self.__client_kill.call_async(target_turtle)
             future.add_done_callback(self.__callback_service_kill)
 
-    def __callback_service_spawn(self, future):
+    def __callback_service_spawn(self, future, request):
         response = future.result()
         self.__spawned_turtles.append(response.name)
+        
+        new_turtle_msg = Pose()
+        new_turtle_msg.x = request.x
+        new_turtle_msg.y = request.y
+
+        self.__publisher_spawned_turtle.publish(new_turtle_msg)
+        
         self.get_logger().info("Turtle named " + str(response.name) + " has been spawned")
 
     def __callback_service_kill(self, future):        
         self.get_logger().info("Turtle has been killed")
+
+    def callback_subscription_killed_turtle(self, Bool):
+        if len(self.__spawned_turtles) > 0:
+            while not self.__client_kill.wait_for_service(1.0):
+                self.get_logger().info("Standing here...")
+
+            target_turtle = Kill.Request()
+            target_turtle.name = self.__spawned_turtles.pop(0)
+
+            future = self.__client_kill.call_async(target_turtle)
+            future.add_done_callback(self.__callback_service_kill)
 
 
 def main(args=None):
