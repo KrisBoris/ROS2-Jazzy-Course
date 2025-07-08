305e747456193509baa588772d62af2fd6a5d226
KrisBoris <k.store@interia.eu>
2025-07-08 18:15:04 +0200

    #17 Turtlesim project
    First working version of C++ steering algorithm; still requires modifications

--- ros2_ws/src/turtlesim_project_cpp/src/turtle_controller.cpp
+++ ros2_ws/src/turtlesim_project_cpp/src/turtle_controller.cpp
@@ -4,6 +4,9 @@
 #include "std_msgs/msg/string.hpp"
 #include "turtlesim/msg/pose.hpp"
 
+// ToDo:
+// When turtle is in theta close to 0.0 it cannot decide which way should he turn
+
 class TurtleControllerNode : public rclcpp::Node
 {
 public:
@@ -14,10 +17,7 @@ public:
         __subscriberTurtlePosition = this->create_subscription<turtlesim::msg::Pose>("/turtle1/pose", 10, 
             std::bind(&TurtleControllerNode::callbackSubscriptionTurtlePosition, this, std::placeholders::_1));
         __subscriberSpawnedTurtle = this->create_subscription<turtlesim_project_interfaces::msg::SpawnedTurtle>("spawned_turtle", 10,
-            std::bind(&TurtleControllerNode::callbackSubscriptionSpawnedTurtle, this, std::placeholders::_1));   
-            
-        prevXDistance = 0.0;
-        prevYDistance = 0.0;
+            std::bind(&TurtleControllerNode::callbackSubscriptionSpawnedTurtle, this, std::placeholders::_1));                       
 
         RCLCPP_INFO(this->get_logger(), "turtle_controller has been created");
     }
@@ -32,106 +32,41 @@ public:
                 __spawnedTurtles.erase(__spawnedTurtles.begin());
             }
             else
-            {
-                // Measure distance and previous distance from turtle to the target
-                // Based on differences betweem current x and y and previous one decide in which quarter, relatively
-                // to the turtle is the target based
-                // Using that choose the turn 
+            {                                   
+                auto cmdVel = geometry_msgs::msg::Twist();                
 
-                auto cmd_vel = geometry_msgs::msg::Twist();
+                double xVec = __spawnedTurtles[0].pose.x - position.x;
+                double yVec = __spawnedTurtles[0].pose.y - position.y;
+                double angle = std::atan2(yVec, xVec);
+
+                RCLCPP_INFO(this->get_logger(), "angle: %f", angle);
+                RCLCPP_INFO(this->get_logger(), "theta: %f", position.theta);    
                 
-                double xSignedDistance = __spawnedTurtles[0].pose.x - position.x;
-                double ySignedDistance = __spawnedTurtles[0].pose.y - position.y;
+                double oppositeThetaAngle = __calcOppositeAngle(position.theta);
 
-                double xDistance = abs(xSignedDistance);
-                double yDistance = abs(ySignedDistance);
-
-                RCLCPP_INFO(this->get_logger(), "X position: %f", position.x);
-                RCLCPP_INFO(this->get_logger(), "Y position: %f", position.y);
-                
-                RCLCPP_INFO(this->get_logger(), "X distance: %f", xDistance);
-                RCLCPP_INFO(this->get_logger(), "Y distance: %f", yDistance);
-
-                RCLCPP_INFO(this->get_logger(), "prev X distance: %f", prevXDistance);
-                RCLCPP_INFO(this->get_logger(), "prev Y distance: %f", prevYDistance);                
-                
-                if(xDistance <= prevXDistance && yDistance <= prevYDistance)
+                if(abs(position.theta - angle) < ANGLES_ALIGNMENT)
                 {
-                    // Target is in the first quarter
-                    
-                    if(xDistance > yDistance + DISTANCE_MARGIN)
-                    {
-                        if((xSignedDistance > 0 && ySignedDistance > 0) || (xSignedDistance < 0 && ySignedDistance < 0))
-                        {
-                            // Turn right (clockwise)
-                            cmd_vel.angular.z = TURNING_RIGHT_SPEED;  
-                            cmd_vel.linear.x = FORWARD_TURNING_SPEED; 
-                        }
-                        else
-                        {
-                            // Turn left (anticlockwise)
-                            cmd_vel.angular.z = TURNING_LEFT_SPEED;  
-                            cmd_vel.linear.x = FORWARD_TURNING_SPEED; 
-                        }
-                                             
-                    }
-                    else if(xDistance < yDistance - DISTANCE_MARGIN)
-                    {
-                        if((xSignedDistance > 0 && ySignedDistance > 0) || (xSignedDistance < 0 && ySignedDistance < 0))
-                        {
-                            // Turn left (anticlockwise)
-                            cmd_vel.angular.z = TURNING_LEFT_SPEED;
-                            cmd_vel.linear.x = FORWARD_TURNING_SPEED;
-                        }   
-                        else
-                        {
-                            // Turn right (clockwise)
-                            cmd_vel.angular.z = TURNING_RIGHT_SPEED;  
-                            cmd_vel.linear.x = FORWARD_TURNING_SPEED; 
-                        }                     
-                    }
-                    else
-                    {
-                        // Only move forward
-                        cmd_vel.linear.x = FORWARD_SPEED;
-                    }                    
-                }                
-                else if(xDistance > prevXDistance && yDistance < prevYDistance)
+                    // Move forward
+                    cmdVel.linear.x = FORWARD_SPEED;
+                    RCLCPP_INFO(this->get_logger(), "forward speed");
+                }
+                else if((angle > position.theta && (position.theta >= 0 || angle < oppositeThetaAngle))
+                    || (angle < position.theta && angle < oppositeThetaAngle))
                 {
-                    // Target is in the second quarter (anticlockwise)
-                    // Keep turnign left (anticlockwise) until target would be in the first quarter
-                    cmd_vel.angular.z = TURNING_LEFT_SPEED;
-                    cmd_vel.linear.x = FORWARD_TURNING_SPEED;                    
-                }                
-                else if(xDistance > prevXDistance && yDistance > prevYDistance)
+                    // Turn left (anticlockwise)
+                    cmdVel.angular.z = TURNING_LEFT_SPEED;
+                    cmdVel.linear.x = FORWARD_TURNING_SPEED;
+                    RCLCPP_INFO(this->get_logger(), "turning left");   
+                }  
+                else
                 {
-                    // Target is in the third quarter (anticlockwise) 
-                    
-                    if(xDistance < yDistance)
-                    {
-                        // Turn right (clockwise)       
-                        cmd_vel.angular.z = TURNING_RIGHT_SPEED;
-                        cmd_vel.linear.x = FORWARD_TURNING_SPEED;                 
-                    }
-                    else 
-                    {
-                        // Turn left (anticlockwise)
-                        cmd_vel.angular.z = TURNING_LEFT_SPEED;
-                        cmd_vel.linear.x = FORWARD_TURNING_SPEED;
-                    }
-                }                
-                else if(xDistance < prevXDistance && yDistance > prevYDistance)
-                {
-                    // Target is in the fourth quarter (anticlockwise)                     
-                    // Keep turnign right (clockwise) until target would be in the first quarter
-                    cmd_vel.angular.z = TURNING_RIGHT_SPEED ;
-                    cmd_vel.linear.x = FORWARD_TURNING_SPEED;
-                }                
-                
-                prevXDistance = xDistance;
-                prevYDistance = yDistance;
-
-                __publisherVelocity->publish(cmd_vel);
+                    // Turn right (clockwise)
+                    cmdVel.angular.z = TURNING_RIGHT_SPEED;
+                    cmdVel.linear.x = FORWARD_TURNING_SPEED;
+                    RCLCPP_INFO(this->get_logger(), "turning right");
+                }                                
+                                
+                __publisherVelocity->publish(cmdVel);
             }
         }        
     }
@@ -155,17 +90,27 @@ private:
     static constexpr double FORWARD_TURNING_SPEED = 0.5;
     static constexpr double TURNING_LEFT_SPEED = 2.0;
     static constexpr double TURNING_RIGHT_SPEED = -2.0;
-    static constexpr double DISTANCE_MARGIN = 0.0;
+    static constexpr double ANGLES_ALIGNMENT = 0.1;
+    static constexpr double PI = 3.14159;
 
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr __publisherVelocity;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr __publisherKilledTurtle;
     rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr __subscriberTurtlePosition;
     rclcpp::Subscription<turtlesim_project_interfaces::msg::SpawnedTurtle>::SharedPtr __subscriberSpawnedTurtle;
 
-    std::vector<turtlesim_project_interfaces::msg::SpawnedTurtle> __spawnedTurtles;
+    std::vector<turtlesim_project_interfaces::msg::SpawnedTurtle> __spawnedTurtles;    
 
-    double prevXDistance;
-    double prevYDistance;
+    double __calcOppositeAngle(double angle)
+    {
+        if(angle > 0)
+        {
+            return angle - PI;
+        }
+        else 
+        {
+            return angle + PI;
+        }
+    }
 };
 
 int main(int argc, char** argv)
