6fcedc63363cfe346fb6552b2fd3b716cb4037a8
KrisBoris <k.store@interia.eu>
2025-07-09 15:27:06 +0200

    #19 Turtlesim project
    Added launch file

---
+++ ros2_ws/build/turtlesim_project_cpp_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/k/Main/Studying/GitHub-Repositories/ROS2-Jazzy-Course/ros2_ws/install/turtlesim_project_interfaces/share/turtlesim_project_interfaces/package.sh"
+. "/home/k/Main/Studying/GitHub-Repositories/ROS2-Jazzy-Course/ros2_ws/install/turtlesim_project_cpp/share/turtlesim_project_cpp/package.sh"
